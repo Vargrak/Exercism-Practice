2c8f13c30f31e76124495c20867d7cb3f33a2b87
Vargrak <thecanterlotcaper@gmail.com>
2022-01-05 11:35:48 -0500

    Update Change.py
    Removed unnecessary start in range loop

--- Python/Change.py
+++ Python/Change.py
@@ -5,7 +5,7 @@ def find_fewest_coins(coins, target, change=None):
     if target < 0:
         raise ValueError("target can't be negative")
     coins.reverse()
-    for i in range(1, target+1):
+    for i in range(target+1):
         for coin_amt in itertools.combinations_with_replacement(coins, i):
             if sum(coin_amt) == target:
                 can_make = 1
