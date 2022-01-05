5f0c4ec0234fce96236c34bcfc702b1019c91333
Vargrak <thecanterlotcaper@gmail.com>
2022-01-05 13:25:14 -0500

    Update Resistor Color.py

--- Python/Resistor Color.py
+++ Python/Resistor Color.py
@@ -1,22 +1,18 @@
-
-
 def color_code(color, lookup=1):
     dict = colors(lookup)
-    return dict[color.capitalize()]
+    return dict[color]
 
 
 def colors(lookup=0):
-    dict = { "Black":0,
-             "Brown":1,
-             "Red":2,
-             "Orange":3,
-             "Yellow":4,
-             "Green":5,
-             "Blue":6,
-             "Violet":7,
-             "Grey":8,
-             "White":9 }
+    dict = { "black":0,
+             "brown":1,
+             "red":2,
+             "orange":3,
+             "yellow":4,
+             "green":5,
+             "blue":6,
+             "violet":7,
+             "grey":8,
+             "white":9 }
     if lookup == 0: return list(dict.keys())
     else: return dict
-
-print(color_code("orange"))
