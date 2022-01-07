4ec6f3507269c8735e94fba756d371aa2211e6dc
Vargrak <thecanterlotcaper@gmail.com>
2022-01-07 17:58:52 -0500

    Update Pythagorean Triplet.py

--- Python/Pythagorean Triplet.py
+++ Python/Pythagorean Triplet.py
@@ -1,24 +1,23 @@
-import itertools
-from functools import cache
+import time
+start = time.time()
 
-@cache
-def triplets_with_sum(number):
-    for m in range(2, number):
-        for n in range(1, m):
-            a = (m**2) - (n**2)
-            b = 2*m*n
-            c = (m**2) + (n**2)
-        print(a, b, c)
-        if a + b + c == number:
-            print(number)
-
-
-
-
-
-
-
-
-    return []
-
-print(triplets_with_sum(1000))
+def triplets_with_sum(number, triplet_list = None, c = 0):
+    triplet_list = []
+    for k in range(1, number):
+            for m in range(2, number):
+                for n in range(1, m):
+                        a = k*((m**2) - (n**2))
+                        b = k*(2*m*n)
+                        c = k*((m**2) + (n**2))
+                        if a + b + c == number:
+                            if a > 0 and b > 0 and c > 0:
+                                temp = sorted([a, b, c])
+                                if temp not in triplet_list:
+                                    triplet_list.append(temp)
+                        if c > number:
+                            break
+                if c > number:
+                        break
+    return sorted(triplet_list)
+print(triplets_with_sum(30000))
+print(time.time() - start)
