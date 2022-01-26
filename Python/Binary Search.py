e38e48c3ada54e7b7611848aa2cc0093580f8f77
Vargrak <Maxwellernst@protonmail.ch>
2022-01-26 12:15:05 -0500

    Update Binary Search.py

--- Python/Binary Search.py
+++ Python/Binary Search.py
@@ -1,12 +1,21 @@
 def find(search_list, value):
+    #check minimum length requirements
     if len(search_list) == 1:
-        return search_list
+        return 0
+    if len(search_list) < 1:
+        raise ValueError("value not in array")
 
-    half = len(search_list)//2
-    if value > half:
-        if value in search_list[half:]:
-            return value
-    elif value <= half:
-        if value in search_list[:half]:
-            return value
+    #find middle position and value
+    half_index = len(search_list)//2
+    half = search_list[half_index]
+    
+    #check upper-half or lower-half of the array 
+    if value >= half:
+        for index, list_value in enumerate(search_list[half_index:]):
+            if list_value == value:
+                return index+len(search_list)//2
+    elif value < half:
+        for index, list_value in enumerate(search_list[:half_index]):
+            if list_value == value:
+                return index
     raise ValueError("value not in array")
