79778feb73feb94b1f24f99a7c6156838b64a745
Vargrak <Maxwellernst@protonmail.ch>
2022-01-31 13:27:53 -0500

    Update D&D Character.py

--- Python/D&D Character.py
+++ Python/D&D Character.py
@@ -6,23 +6,19 @@ class Character:
         self.UPPER = 18
         self.generator()
 
-    def modifier(self, ability_score):
-        return (ability_score-10)//2
-
     def generator(self):
-        self.strength = self.ability
-        self.dexterity = self.ability
-        self.constitution = self.ability
-        self.intelligence = self.ability
-        self.wisdom = self.ability
-        self.charisma = self.ability
-        self.hitpoints = (10+self.modifier(self.constitution))
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = (10+modifier(self.constitution))
     
-    @property
     def ability(self):
         rolls = [random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), random.randint(1, 6)]
         rolls = sorted(rolls)
         return sum(rolls[1:])
 
-print(Character().constitution)
-print(Character().hitpoints)
+def modifier(ability_score):
+        return (ability_score-10)//2
