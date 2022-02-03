84540bf9050a98d5f5b5d4ea91685823d040bad9
Vargrak <Maxwellernst@protonmail.ch>
2022-02-02 21:14:20 -0500

    Update List Ops.py
    Added comments

--- Python/List Ops.py
+++ Python/List Ops.py
@@ -1,12 +1,10 @@
 def append(list1, list2):
-    appended_list = []
-    for item in list1:
-        appended_list += [item]
-    for item in list2:
-        appended_list += [item]
+    """Adds two lists together by adding each of the lists together"""
+    appended_list = list1 + list2
     return appended_list
 
 def concat(lists):
+    """Flattens one layer in a list of lists down. For every item in the list it takes everything one layer down and adds it into a new list."""
     concat_list = []
     for list_ in lists:
         for x in list_:
@@ -14,6 +12,7 @@ def concat(lists):
     return concat_list
 
 def filter(function, list_):
+    """Function inserted is a lambda that returns a bool. It compares against each item in the input list to determine if it is added to the output list."""
     filtered_list = []
     for x in list_:
         if function(x) == True:
@@ -21,28 +20,33 @@ def filter(function, list_):
     return filtered_list
 
 def length(list_):
+    """Simply cycles through every item and increments count. Result should be length of the list."""
     count = 0
     for item in list_:
         count += 1
     return count
 
 def map(function, list_):
+    """Applies a lambda function to each item in the list provided and outputs the results in a new list."""
     mapped_list = []
     for x in list_:
         mapped_list += [function(x)]
     return mapped_list
 
 def foldl(function, list_, initial):
+    """For every item in a list, take an initial value and apply a lambda function to that item. Result is the new value of 'initial'"""
     for x in list_:
        initial = function(initial, x)
     return initial
         
 def foldr(function, list_, initial):
+    """Same as foldl but reversed."""
     for x in list_[::-1]:
         initial = function(x, initial)
     return initial
 
 def reverse(list_):
+    """Takes a list and parses it in reverse to append it to a new, reversed list."""
     reversed_list = []
     for item in list_[::-1]:
        reversed_list += [item]
