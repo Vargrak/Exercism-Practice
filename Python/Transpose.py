4787ff3c7fbf2994bb6b3dbe5c793633966941c5
Vargrak <Maxwellernst@protonmail.ch>
2022-02-14 14:15:52 -0500

    Update Transpose.py
    Finally finished. Got around spaces too many or too few spaces (due to rstrip) by altering spaces to other text before zipping

--- Python/Transpose.py
+++ Python/Transpose.py
@@ -1,11 +1,8 @@
-#WIP
 from itertools import zip_longest
 
 def transpose(lines):
-    rows = lines.split("\n")
-    tupled_list = list(zip_longest(*rows, fillvalue=" "))
-    detupled_list = [''.join(item) for item in tupled_list]
-    return '\n'.join(detupled_list)
-
-
-print(transpose("\n".join(lines)))
+    lines = lines.replace(" ", "_")
+    tupled_list = list(zip_longest(*lines.split("\n"), fillvalue=" "))
+    detupled_list = [''.join(item).rstrip() for item in tupled_list]
+    transposed = '\n'.join(detupled_list).replace("_", " ")
+    return transposed
