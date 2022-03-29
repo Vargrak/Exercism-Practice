646a2a757467c8a4b1be4c2e9fbdbe6dedfa9896
Vargrak <Maxwellernst@protonmail.ch>
2022-03-29 09:49:22 -0400

    Update Bird Watcher.cs

--- C#/Bird Watcher.cs
+++ C#/Bird Watcher.cs
@@ -3,9 +3,7 @@ using System;
 class BirdCount
 {
     private int[] birdsPerDay;
-
-    private int[] birdsLastWeek = new int[0, 2, 5, 3, 7, 8, 4];
-
+    
     public BirdCount(int[] birdsPerDay)
     {
         this.birdsPerDay = birdsPerDay;
@@ -13,26 +11,52 @@ class BirdCount
 
     public static int[] LastWeek()
     {
-        return this.birdsLastWeek;
+        int[] LWBirds = {0, 2, 5, 3, 7, 8, 4};
+        return LWBirds;
     }
 
     public int Today()
     {
-        
+        return this.birdsPerDay[birdsPerDay.Length - 1];
     }
 
     public void IncrementTodaysCount()
     {
-
+        this.birdsPerDay[birdsPerDay.Length - 1]++;
     }
 
     public bool HasDayWithoutBirds()
     {
+        if (Array.Exists(birdsPerDay, x => x == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
+    public int CountForFirstDays(int numberofDays)
+    {
+        int count = 0;
+        for (int i = 0; i < numberofDays; i++)
+        {
+            count += this.birdsPerDay[i];
+        }
+        return count;
     }
 
     public int BusyDays()
     {
-
+        int count = 0;
+        for (int i = 0; i < this.birdsPerDay.Length; i++)
+        {
+            if (this.birdsPerDay[i] >= 5)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
