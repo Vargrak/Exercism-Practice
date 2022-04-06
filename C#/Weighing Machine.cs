7eeffb9bfde951790816e807e9b144554bc6b50e
Vargrak <thecanterlotcaper@gmail.com>
2022-04-06 14:31:54 -0400

    Create Weighing Machine.cs
    WIP - Initial Commit

---
+++ C#/Weighing Machine.cs
@@ -0,0 +1,19 @@
+using System;
+
+class WeighingMachine
+{
+    // Constructor:
+    // TODO: define the 'Precision' property
+
+    // TODO: define the 'Weight' property
+
+    // TODO: define the 'DisplayWeight' property
+
+    // TODO: define the 'TareAdjustment' property
+
+    public WeighingMachine(int precision)
+    {
+        this.Precision = precision;
+    }
+
+}
