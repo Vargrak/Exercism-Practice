57339feac3cb079cb2e1e1fc794e8d5e42e7a7c9
Vargrak <Maxwellernst@protonmail.ch>
2022-04-06 17:11:26 -0400

    Create The Weather in Deather.cs

---
+++ C#/The Weather in Deather.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+
+public class WeatherStation
+{
+    private Reading reading;
+    private List<DateTime> recordDates = new List<DateTime>();
+    private List<decimal> temperatures = new List<decimal>();
+
+    public void AcceptReading(Reading reading)
+    {
+        this.reading = reading;
+        recordDates.Add(DateTime.Now);
+        temperatures.Add(reading.Temperature);
+    }
+
+    public void ClearAll()
+    {
+        reading = new Reading();
+        recordDates.Clear();
+        temperatures.Clear();
+    }
+
+    public decimal LatestTemperature { get => reading.Temperature; }
+ 
+    public decimal LatestPressure { get => reading.Pressure; }
+
+    public decimal LatestRainfall { get => reading.Rainfall; }
+
+    public bool HasHistory { get => recordDates.Count > 1; }
+
+    public Outlook ShortTermOutlook
+    {
+        get
+        {
+            return reading.Temperature switch
+            {
+                _ when reading.Equals(new Reading()) => throw new ArgumentException("No readings"),
+                _ when reading.Pressure < 10m && reading.Temperature < 30m => Outlook.Cool,
+                _ when reading.Temperature > 50m => Outlook.Good,
+                _ => Outlook.Warm,
+            };
+        }
+    }
+
+    public Outlook LongTermOutlook
+    {
+        get
+        {
+            return reading.WindDirection switch
+            {
+                WindDirection.Northerly => Outlook.Cool,
+                WindDirection.Southerly => Outlook.Good,
+                WindDirection.Easterly => (reading.Temperature <= 20) ? Outlook.Warm : Outlook.Good,
+                WindDirection.Westerly => Outlook.Rainy,
+                _ => throw new ArgumentException("Invalid wind direction"),
+            };
+        }
+    }
+
+    public State RunSelfTest() => reading.Equals(new Reading()) ? State.Bad : State.Good;
+
+}
+
+/*** Please do not modify this struct ***/
+public struct Reading
+{
+    public decimal Temperature { get; }
+    public decimal Pressure { get; }
+    public decimal Rainfall { get; }
+    public WindDirection WindDirection { get; }
+
+    public Reading(decimal temperature, decimal pressure,
+        decimal rainfall, WindDirection windDirection)
+    {
+        Temperature = temperature;
+        Pressure = pressure;
+        Rainfall = rainfall;
+        WindDirection = windDirection;
+    }
+}
+
+/*** Please do not modify this enum ***/
+public enum State
+{
+    Good,
+    Bad
+}
+
+/*** Please do not modify this enum ***/
+public enum Outlook
+{
+    Cool,
+    Rainy,
+    Warm,
+    Good
+}
+
+/*** Please do not modify this enum ***/
+public enum WindDirection
+{
+    Unknown, // default
+    Northerly,
+    Easterly,
+    Southerly,
+    Westerly
+}
