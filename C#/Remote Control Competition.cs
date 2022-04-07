d3a7e8421a822a7b721f9c410003f8ad9ec80685
Vargrak <Maxwellernst@protonmail.ch>
2022-04-07 17:07:07 -0400

    Create Remote Control Competition.cs

---
+++ C#/Remote Control Competition.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+public interface IRemoteControlCar
+{
+    void Drive();
+    int DistanceTravelled { get; }
+}
+
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable<ProductionRemoteControlCar>
+{
+    public int DistanceTravelled { get; private set; }
+    public int NumberOfVictories { get; set; }
+
+    public void Drive() => DistanceTravelled += 10;
+
+    public int CompareTo(ProductionRemoteControlCar other) => NumberOfVictories.CompareTo(other.NumberOfVictories);
+}
+
+public class ExperimentalRemoteControlCar : IRemoteControlCar
+{
+    public int DistanceTravelled { get; private set; }
+
+    public void Drive() => DistanceTravelled += 20;
+}
+
+public static class TestTrack
+{
+    public static void Race(IRemoteControlCar car) => car.Drive();
+
+    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
+        ProductionRemoteControlCar prc2) => (prc2.NumberOfVictories > prc1.NumberOfVictories) ? new List<ProductionRemoteControlCar> { prc1, prc2 } : new List<ProductionRemoteControlCar> { prc2, prc1 };
+}
