8a386a74d2c38af8acecdc372551d387b7fe9f1a
Vargrak <Maxwellernst@protonmail.ch>
2022-04-12 17:29:15 -0400

    Create Orm in One Go.cs

---
+++ C#/Orm in One Go.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Orm
+{
+    private Database database;
+
+    public Orm(Database database) => this.database = database;
+
+    public void Write(string data)
+    {
+        using Database db = database;
+        {
+            db.BeginTransaction();
+            db.Write(data);
+            db.EndTransaction();
+        }
+    }
+
+    public bool WriteSafely(string data)
+    {
+        using Database db = database;
+        {
+            try
+            {
+                Write(data);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
