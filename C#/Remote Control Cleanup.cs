9d9917fe1025a8d5e60b48475057b7cc0debdd91
Vargrak <Maxwellernst@protonmail.ch>
2022-04-13 15:20:56 -0400

    Update Remote Control Cleanup.cs
    Not the best, but it worked

--- C#/Remote Control Cleanup.cs
+++ C#/Remote Control Cleanup.cs
@@ -1,6 +1,37 @@
-//WIP
 public class RemoteControlCar
 {
+    public RemoteControlCar()
+    {
+        CurrentSponsor = "";
+        Telemetry = new CarTelemetry(this);
+    }
+    public enum SpeedUnits
+    {
+        MetersPerSecond,
+        CentimetersPerSecond
+    }
+
+    public struct Speed
+    {
+        public decimal Amount { get; }
+        public SpeedUnits SpeedUnits { get; }
+
+        public Speed(decimal amount, SpeedUnits speedUnits)
+        {
+            Amount = amount;
+            SpeedUnits = speedUnits;
+        }
+
+        public override string ToString()
+        {
+            return SpeedUnits switch
+            {
+                SpeedUnits.CentimetersPerSecond => $"{Amount} centimeters per second",
+                _ => $"{Amount} meters per second"
+            };
+        }
+    }
+
     public string CurrentSponsor { get; private set; }
     public ITelemetry Telemetry { get; private set; }
 
@@ -11,62 +42,39 @@ public class RemoteControlCar
     public void SetSponsor(string sponsorName) => CurrentSponsor = sponsorName;
 
     public void SetSpeed(Speed speed) => currentSpeed = speed;
-}
 
-public interface ITelemetry
-{
-    public void Calibrate();
-    public bool SelfTest();
-    public void ShowSponsor(string sponsorName);
-    public void SetSpeed(decimal amount, string unitsString);
-}
-
-public class Telemetry : ITelemetry
-{
-    public string unitsString { get; set; }
-    public SpeedUnits SpeedUnits { get; set; }
-    public RemoteControlCar Car { get; set; }
-
-    public void Calibrate(){}
-
-    public bool SelfTest() =>true;
-    
-    public void ShowSponsor(string sponsorName) => Car.SetSponsor(sponsorName);
-
-    public void SetSpeed(decimal amount, string unitsString="")
+    public interface ITelemetry
     {
-        SpeedUnits speedUnits = unitsString switch
+        public void Calibrate();
+        public bool SelfTest();
+        public void ShowSponsor(string sponsorName);
+        public void SetSpeed(decimal amount, string unitsString);
+    }
+
+    public class CarTelemetry : ITelemetry
+    {
+        public CarTelemetry(RemoteControlCar car) => Car = car;
+
+        public RemoteControlCar Car { get; }
+        public string unitsString { get; set; }
+        public SpeedUnits SpeedUnits { get; set; }
+
+        public void Calibrate(){}
+
+        public bool SelfTest() =>true;
+        
+        public void ShowSponsor(string sponsorName) => Car.SetSponsor(sponsorName);
+
+        public void SetSpeed(decimal amount, string unitsString="")
         {
-            "cps" => SpeedUnits.CentimetersPerSecond,
-            _ => SpeedUnits.MetersPerSecond
-        };
-        Car.SetSpeed(new Speed(amount, speedUnits));
+            SpeedUnits speedUnits = unitsString switch
+            {
+                "cps" => SpeedUnits.CentimetersPerSecond,
+                _ => SpeedUnits.MetersPerSecond
+            };
+            Car.SetSpeed(new Speed(amount, speedUnits));
+        }
     }
 }
 
-public enum SpeedUnits
-{
-    MetersPerSecond,
-    CentimetersPerSecond
-}
 
-public struct Speed
-{
-    public decimal Amount { get; }
-    public SpeedUnits SpeedUnits { get; }
-
-    public Speed(decimal amount, SpeedUnits speedUnits)
-    {
-        Amount = amount;
-        SpeedUnits = speedUnits;
-    }
-
-    public override string ToString()
-    {
-        return SpeedUnits switch
-        {
-            SpeedUnits.CentimetersPerSecond => $"{Amount} centimeters per second",
-            _ => $"{Amount} meters per second"
-        };
-    }
-}
