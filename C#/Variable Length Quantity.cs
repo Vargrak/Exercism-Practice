defefb3c2b0605e33f2f2af5f3f80958417b2d43
Vargrak <Maxwellernst@protonmail.ch>
2022-04-28 12:43:12 -0400

    Update Variable Length Quantity.cs

--- C#/Variable Length Quantity.cs
+++ C#/Variable Length Quantity.cs
@@ -1,34 +1,52 @@
 using System;
 using System.Collections.Generic;
-// WIP - Issues with bit shifting and reversed on some byte values
 public static class VariableLengthQuantity
 {
     public static uint[] Encode(uint[] numbers)
     {
+        Array.Reverse(numbers);
         List<uint> encoded_VLQ = new List<uint>();
-        if (numbers[0] == 0)
-            return numbers;
         
         uint temp = 0;
         foreach (uint number in numbers)
-            temp += number;
-        
-
-        uint msb = 0x00;
-        while (temp > 0)
         {
-            uint encoded_number = (msb ^ (temp & 0x7F));
-            temp >>= 7;
-            Console.WriteLine(Convert.ToString(encoded_number, 2));
-            encoded_VLQ.Insert(0, encoded_number);
-            if (msb == 0x00)
-                msb = 0x80;
+            if (number != 0)
+            {
+                temp = number;
+                uint msb = 0b00000000;
+                while (temp > 0)
+                {
+                    uint encoded_number = (msb ^ (temp & 0b01111111));
+                    temp >>= 7;
+                    encoded_VLQ.Insert(0, encoded_number);
+                    if (msb == 0x00)
+                        msb = 0b10000000;
+                }
+            }
+            else
+                encoded_VLQ.Insert(0, number);
         }
         return encoded_VLQ.ToArray();
     }
 
     public static uint[] Decode(uint[] bytes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (bytes[bytes.Length - 1] != 0 && bytes[bytes.Length - 1] != 127)
+            throw new InvalidOperationException("Last byte error");
+
+        List<uint> decoded_VLQ = new List<uint>();
+        uint temp = 0;
+        foreach (uint byte_value in bytes)
+        {
+            temp <<= 7;
+            temp |= (byte_value & 0b01111111);
+            if ((byte_value & 0b10000000) == 0)
+            {
+                decoded_VLQ.Add(temp);
+                temp = 0;
+            }
+        }
+        Array.Reverse(decoded_VLQ.ToArray());
+        return decoded_VLQ.ToArray();
     }
 }
