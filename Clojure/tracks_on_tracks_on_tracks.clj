dadb85ae9c5ddd0e714799e9b19115f78e641dcb
Vargrak <Maxwellernst@protonmail.ch>
2022-11-17 17:54:56 -0500

    Not finished tracks_on_tracks

---
+++ Clojure/tracks_on_tracks_on_tracks.clj
@@ -0,0 +1,32 @@
+(ns tracks-on-tracks-on-tracks)
+
+(defn new-list []
+  "Creates an empty list of languages to practice."
+  (list))
+
+(defn add-language [lang-list lang]
+  "Adds a language to the list."
+  (cons lang lang-list))
+
+(defn first-language [lang-list]
+  "Returns the first language on the list."
+  (first lang-list))
+
+(defn remove-language [lang-list]
+  "Removes the first language added to the list."
+  (rest lang-list))
+
+(defn count-languages [lang-list]
+  "Returns the total number of languages on the list."
+  (count lang-list))
+
+(defn learning-list []
+  "Creates an empty list, adds Clojure and Lisp, removes Lisp, adds
+  Java and JavaScript, then finally returns a count of the total number
+  of languages."
+  (count-languages
+   (add-language "JavaScript"
+                 (add-language "Java"
+                               (remove-language
+                                (add-language "Lisp"
+                                              (add-language "Clojure" (new-list))))))))
